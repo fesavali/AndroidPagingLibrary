f76f397a295bf2a32b91eec33cbe9235fd239104
fesavali <felixnzioki99@gmail.com>
2021-10-26 16:02:48 +0300

    implement data source class

---
+++ app/src/main/java/com/savalicodes/paginglibrary/dataSources/PagedCountriesDataSource.kt
@@ -0,0 +1,21 @@
+package com.savalicodes.paginglibrary.dataSources
+
+import androidx.paging.PageKeyedDataSource
+import com.savalicodes.paginglibrary.models.Country
+
+class PagedCountriesDataSource: PageKeyedDataSource<Int, Country>() {
+    override fun loadInitial(
+        params: LoadInitialParams<Int>,
+        callback: LoadInitialCallback<Int, Country>
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Country>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Country>) {
+        TODO("Not yet implemented")
+    }
+}
