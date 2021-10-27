7b0a7b05ae87c2096dd4a1da3f7bf303947bc1c3
fesavali <felixnzioki99@gmail.com>
2021-10-27 13:28:12 +0300

    add page and id variables

--- app/src/main/java/com/savalicodes/paginglibrary/models/Country.kt
+++ app/src/main/java/com/savalicodes/paginglibrary/models/Country.kt
@@ -1,9 +1,11 @@
 package com.savalicodes.paginglibrary.models
 
 data class Country (
+    var id: Int,
          var name: String,
          var countryCode: String,
          var shortCode: String,
          var population: String,
          var area: String,
+         var page: Int,
     )
