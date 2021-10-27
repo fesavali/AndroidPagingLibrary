e7a24bb9c6a4a1a3167721d52e7d6ae86960fa26
fesavali <felixnzioki99@gmail.com>
2021-10-27 16:27:37 +0300

    create view model connect to main activity

--- app/src/main/java/com/savalicodes/paginglibrary/MainActivity.kt
+++ app/src/main/java/com/savalicodes/paginglibrary/MainActivity.kt
@@ -2,6 +2,12 @@ package com.savalicodes.paginglibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.savalicodes.paginglibrary.models.Country
 import com.savalicodes.paginglibrary.utils.CountriesDb
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +16,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         CountriesDb.initialize(this)
+
+        val countryRV = findViewById<RecyclerView>(R.id.countrier_rv)
+        val adapter = CountryAdapter()
+         countryRV.layoutManager = LinearLayoutManager(this)
+        countryRV.adapter = adapter
+
+        var viewModel = MainActivityViewModel()
+        viewModel.countries.observe(this, { countries->
+                Log.i("Main data", "Observed ${countries.size} countries from ViewModel...")
+            adapter.submitList(countries)
+        })
     }
 }
---
+++ app/src/main/java/com/savalicodes/paginglibrary/MainActivityViewModel.kt
@@ -0,0 +1,23 @@
+package com.savalicodes.paginglibrary
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.savalicodes.paginglibrary.dataSources.PagedCountriesDataSourceFactory
+import com.savalicodes.paginglibrary.models.Country
+
+class MainActivityViewModel : ViewModel() {
+
+    val pageSize = 15
+    var config = PagedList.Config.Builder()
+        .setPageSize(pageSize)
+        .setInitialLoadSizeHint(15)
+        .setPrefetchDistance(5)
+        .setEnablePlaceholders(false)
+        .build()
+    var dataSource = PagedCountriesDataSourceFactory()
+    var countries : LiveData<PagedList<Country>> = LivePagedListBuilder(dataSource, config).build()
+
+
+}
