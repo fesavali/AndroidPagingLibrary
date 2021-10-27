0319a0eaaac0dadba104a205c806f9cb00e546ef
fesavali <felixnzioki99@gmail.com>
2021-10-27 14:45:18 +0300

    implement loadRange override

--- app/src/main/java/com/savalicodes/paginglibrary/dataSources/PositionalCountryDataSource.kt
+++ app/src/main/java/com/savalicodes/paginglibrary/dataSources/PositionalCountryDataSource.kt
@@ -1,17 +1,39 @@
 package com.savalicodes.paginglibrary.dataSources
 
+import android.util.Log
 import androidx.paging.PositionalDataSource
 import com.savalicodes.paginglibrary.models.Country
 import com.savalicodes.paginglibrary.utils.CountriesDb
 
 class PositionalCountryDataSource: PositionalDataSource<Country>() {
-    private val TAG: String = "KeyedCountriesDataSource"
+    private val TAG: String = "PositionalCountriesDataSource"
     private val source = CountriesDb.getCountries()
+    private var batchSize = 0
     override fun loadInitial(params: LoadInitialParams, callback: LoadInitialCallback<Country>) {
-        TODO("Not yet implemented")
+       Log.v(TAG, "loadInitial called")
+        batchSize = params.requestedLoadSize
+        val list = mutableListOf<Country>()
+        for (i in params.requestedStartPosition..params.requestedLoadSize){
+            if (i == source.size){
+                break
+            }
+            list.add(source.get(i))
+        }
+        Log.v(TAG, "loadInitial created a list of ${list.size} items..")
+        callback.onResult(list.orEmpty(), params.requestedStartPosition, list.size)
     }
 
     override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<Country>) {
-        TODO("Not yet implemented")
+        Log.v(TAG, "loadRange called")
+        val list = mutableListOf<Country>()
+        for (i in params.startPosition..(params.startPosition + batchSize)){
+            if (i == source.size){
+                break
+            }
+            list.add(source.get(i))
+        }
+        Log.v(TAG, "loadRange created a list of ${list.size} items..")
+        callback.onResult(list.orEmpty())
     }
+
 }
