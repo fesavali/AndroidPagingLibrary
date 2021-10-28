4c27e7f0c858330398fb6c07e5ec153428cdda9b
fesavali <felixnzioki99@gmail.com>
2021-10-28 14:59:43 +0300

    update code to handle dao

--- app/src/main/java/com/savalicodes/paginglibrary/MainActivityViewModel.kt
+++ app/src/main/java/com/savalicodes/paginglibrary/MainActivityViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.ViewModel
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import androidx.paging.toLiveData
+import com.afzaalahmadzeeshan.android.paging.data.ContactDatabase
 import com.savalicodes.paginglibrary.dataSources.PagedCountriesDataSourceFactory
+import com.savalicodes.paginglibrary.models.Contact
 import com.savalicodes.paginglibrary.models.Country
 
 class MainActivityViewModel : ViewModel() {
@@ -23,12 +25,32 @@ class MainActivityViewModel : ViewModel() {
         .setBoundaryCallback(CountriesBoundaryCallback())
         .build()
 
-
+//
 //    use this when using paging.runtime-kxt on gradle
 //    var countries : LiveData<PagedList<Country>> = dataSource
 //        .toLiveData(config,
 //        false,
 //        CountriesBoundaryCallback()
 //    )
+//using dao
+//class MainActivityViewModel(db: ContactDatabase): ViewModel() {
+//    val pageSize = 15
+//    var config = PagedList.Config.Builder()
+//        .setPageSize(pageSize)
+//        .setInitialLoadSizeHint(15)
+//        .setPrefetchDistance(5)
+//        .setEnablePlaceholders(false)
+//        .build()
+//    var dataSource = db.contactDao()
+//
+//        var countries : LiveData<PagedList<Contact>> = LivePagedListBuilder(dataSource, config)
+//        .setBoundaryCallback(ContactsBoundaryCallback)
+//        .build()
+
+//    var contacts: LiveData<PagedList<Contact>> = dataSource.getContacts()
+//        .toLiveData(config,
+//            null,
+//            ContactsBoundaryCallback()
+//        )
 
 }
